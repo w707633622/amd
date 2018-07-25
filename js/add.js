c5a9da9e25a819d04ba4973fc5151c8e26fdcae0
w707633622 <w707633622@vip.qq.com>
2018-07-25 18:07:39 +0800

    模块开发

---
+++ js/add.js
@@ -0,0 +1,9 @@
+/*AMD规范*/
+define(function(){
+	var add = function(x,y){
+		return x + y;
+	}
+	return{
+		add:add;
+	}
+})
---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*配置路径*/
+/*require.config({
+	path:{
+		模块的名字:模块的路径
+	}
+})*/
+require.config({
+	path:{
+		"add":"add"
+	}
+})
